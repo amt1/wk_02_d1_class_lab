e2311974c7479217a88c4c7d831ef361dc640a5b
Annabel Treshansky <annabelt@Annabels-MacBook-Air.local>
2019-03-04 18:41:07 +0000

    Week 2 Day 1 homework: classes

---
+++ library_class.rb
@@ -0,0 +1,71 @@
+class Library
+
+  attr_accessor :books
+  def initialize(books)
+    @library_books = books
+  end
+
+  def get_books
+    return @library_books
+  end
+
+  def get_book_by_title(title)
+    bookfound=nil
+    for book in @library_books
+      bookfound = book if ( book[:title] == title )
+    end
+    return bookfound
+  end
+
+  def get_rental_details_by_title(title)
+    rental_details=nil
+    for book in @library_books
+#      rental_details = book[:rental_details] if ( book[:title] == title )
+    # ok it's going to go through the whole loop but I only have a library of 2...
+# fix that long loop!
+      return book[:rental_details] if ( book[:title] == title )
+    end
+    return rental_details
+  end
+
+  def add_new_book(new_book)
+#    p "New book: #{new_book}"
+    @library_books << new_book if (new_book != nil)
+#    p "New Library:  #{@library_books}"
+  end
+
+  # def add_new_library_book(new_library_book)
+  #     p "New book: #{new_library_book}"
+  #   @library_books << new_library_book if (new_library_book != nil)
+  #     p "New Library:  #{@library_books}"
+  # end
+
+
+
+  def update_details(title,name,date)
+    mybook=get_book_by_title(title)
+#    p mybook
+    new_rental_details=Hash.new()
+    new_rental_details[:student_name]=name
+    new_rental_details[:date]=date
+  #  p new_rental_details
+    array_position=@library_books.index(mybook)
+#    p array_position
+    @library_books[array_position][:rental_details]=new_rental_details
+#    p @library_books
+  end
+
+end # end class
+
+## # TODO: new class - kind of thing we'll do tomorrow?
+# class LibraryBook
+#
+# attr_reader :title
+# attr_accessor :rental_details
+#
+# def initialize(title, student, date)
+#   @title = title
+#   @rental_details={ student_name: student, due_date: date  }
+# end
+#
+# end # end class
---
+++ specs/library_spec.rb
@@ -0,0 +1,71 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../library_class')
+
+class TestLibraryClass < MiniTest::Test
+
+def setup
+
+  @books=[
+    {
+      title: "Lord of the Rings",
+      rental_details: {
+       student_name: "Trevis",
+       date: "01/04/19"
+      }
+    },
+      {
+        title: "The Shipping News",
+        rental_details: {
+         student_name: "Flotter",
+         date: "02/04/19"
+        }
+      }
+  ]
+end
+
+def test_get_books
+  mylibrary=Library.new(@books)
+  assert_equal( @books, mylibrary.get_books )
+end
+
+def test_get_book_by_title
+  mylibrary=Library.new(@books)
+  mybook = mylibrary.get_book_by_title("The Shipping News")
+  assert_equal( @books[1], mybook )
+end
+
+def test_get_rental_details_by_title
+  mylibrary=Library.new(@books)
+  my_book_rental_details = mylibrary.get_rental_details_by_title( "Lord of the Rings" )
+  assert_equal( @books[0][:rental_details], my_book_rental_details )
+end
+
+def test_add_new_book
+  mylibrarybook={title: "The Gruffalo Cookbook", rental_details: {student_name: "", date: ""}}
+  mylibrary=Library.new(@books)
+  mylibrary.add_new_book(mylibrarybook)
+assert_equal( @books[-1][:title], mylibrarybook[:title])
+assert_equal( @books[-1][:title], "The Gruffalo Cookbook")
+end
+
+
+def test_update_rental_details
+    mylibrary=Library.new(@books)
+   mylibrary.update_details("The Shipping News","Rolf","01/05/19")
+   check_new_rental_details=mylibrary.get_rental_details_by_title("The Shipping News")
+   assert_equal("Rolf",check_new_rental_details[:student_name])
+end
+
+## # TODO: new library book class
+# def test_add_new_library_book
+#   mylibrarybook=LibraryBook.new("The Gruffalo Cookbook",nil,nil)
+#   mylibrary=Library.new(@books)
+#   mylibrary.add_new_library_book(mylibrarybook)
+# assert_equal( @books[-1][:title], mylibrarybook[:title])
+# assert_equal( @books[-1][:title], "The Gruffalo Cookbook")
+# end
+
+
+
+end # end class
